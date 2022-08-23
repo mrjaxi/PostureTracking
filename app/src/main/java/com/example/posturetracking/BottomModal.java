08b7c6c5124a01773650df6da622eee8f8f17ec5
Sergey <jaxi@atma.company>
2022-08-23 17:02:57 +0500

    make modal window

---
+++ app/src/main/java/com/example/posturetracking/BottomModal.java
@@ -0,0 +1,14 @@
+package com.example.posturetracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BottomModal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_modal);
+    }
+}
